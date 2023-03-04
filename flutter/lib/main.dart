fc7b5c1ae76f5bbc6f2d41a6d67dc1655a725306
kimkiyong <fenderst0612@gmail.com>
2023-03-04 22:22:55 +0900

    feat: size, background, padding, border

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -8,24 +8,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(color: Colors.blue, child: Text('blue')),
+              Container(
+                width: 200,
+                height: 50,
+                color: Colors.blue,
+                child: Text('200x50'),
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                color: Colors.blue,
+                child: Text('padding'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 2),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: EdgeInsets.all(8),
+                child: Text('border'),
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage('https://placehold.jp/200x100.png'),
+                    ),
+                  ),
+                  width: 200,
+                  height: 100,
+                  child: Text('iamge'))
+            ],
+          ),
         ),
-        home: Scaffold(
-            body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Text('Default'),
-            Text('Boid', style: TextStyle(fontWeight: FontWeight.bold)),
-            Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
-            Text('fontSize = 36', style: TextStyle(fontSize: 36)),
-            Text('Red', style: TextStyle(color: Colors.red)),
-            Container(
-                width: double.infinity,
-                color: Colors.grey,
-                child: Text('TextAlign.right', textAlign: TextAlign.right))
-          ],
-        )));
+      ),
+    );
   }
 }
